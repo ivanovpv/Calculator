70940ace5d255fe88dbffec66a07f134ec0c5606
ivanovpv <ivanovpv@gmail.com>
2019-11-06 23:09:56 +0300

    onClick button0

--- app/src/main/java/ru/vultuc/android/calculator/MainActivity.java
+++ app/src/main/java/ru/vultuc/android/calculator/MainActivity.java
@@ -3,12 +3,32 @@ package ru.vultuc.android.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private static String TAG=MainActivity.class.getName();
+    private TextView textLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textLine=this.findViewById(R.id.textLine);
+        Button button0=this.findViewById(R.id.button0);
+        button0.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.button0:
+                textLine.setText("1");
+                break;
+            default:
+                Log.i(TAG, "Pressed button with id="+view.getId());
+        }
     }
 }
